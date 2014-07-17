c0cd41bbfddd2d02ecb4ff34908cb4744cf6a947
canito0890 <joel.ca@feathermexico.com>
2014-07-17 16:02:52 -0400

    bug fix

--- app/controllers/kms_controller.rb
+++ app/controllers/kms_controller.rb
@@ -1,6 +1,6 @@
 class KmsController < FrontController
   
-  before_filter :assert_ajax_post, only: [:select]
+  #before_filter :assert_ajax_post, only: [:select]
   before_filter :assert_km, only: [:show, :show_action, :select]
   before_filter :assert_kms, only: [:compare, :stats]
   before_filter :page_elements, only: [:show]
